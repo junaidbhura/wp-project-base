125c639d99e2099a0d2be8d6a3311e54d3452fb9
Junaid Bhura <info@junaidbhura.com>
2018-05-29 11:39:35 +0930

    Update bootstrap function

--- wp-content/mu-plugins/fooclient/bootstrap.php
+++ wp-content/mu-plugins/fooclient/bootstrap.php
@@ -12,7 +12,7 @@ namespace FooClient;
  */
 function bootstrap() {
 
-	load_module( 'bar', 'FooClient\Bar' );
+	load_module( 'bar' );
 
 }
 
@@ -20,15 +20,19 @@ function bootstrap() {
  * Load a module.
  *
  * @param string $dir The module's directory.
- * @param string $namespace The module's namespace.
  * @param string $hook The hook to use for the module's setup function.
+ * @param string $namespace The module's namespace.
  */
-function load_module( $dir = '', $namespace = '', $hook = 'plugins_loaded' ) {
+function load_module( $dir = '', $hook = 'plugins_loaded', $namespace = '' ) {
 	$path = __DIR__ . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $dir . '.php';
 	if ( file_exists( $path ) ) {
 		require_once $path;
 
-		if ( ! empty( $namespace ) && ! empty( $hook ) ) {
+		if ( ! empty( $hook ) ) {
+			if ( empty( $namespace ) ) {
+				$namespace = __NAMESPACE__ . '\\' . str_replace( ' ', '', ucwords( str_replace( '-', ' ', $dir ) ) );
+			}
+
 			$setup_function = $namespace . '\setup';
 			if ( function_exists( $setup_function ) ) {
 				add_action( $hook, $setup_function );
