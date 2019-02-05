2e157615f83002c74a813ab38d037e0838a6125a
Junaid Bhura <info@junaidbhura.com>
2019-02-05 12:00:46 +1030

    Oops

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = ( env ) => {
 					use: [
 						MiniCssExtractPlugin.loader,
 						{
-							loader: css-loader,
+							loader: 'css-loader',
 							options: {
 								url: false,
 							},
